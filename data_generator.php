290902ecac9e63c6b2e4cc6f1240b5a0ae4c77a0
leozinho0_w <leandroviana28@gmail.com>
2016-11-10 23:05:51 -0200

    Alteracoes em alguns datasets Json. Inclusao de itens

---
+++ data_generator.php
@@ -0,0 +1,50 @@
+<?php
+
+
+//for time
+/*
+	for($ano = 1992; $ano <= 2016; $ano++){
+		for($mes = 1; $mes <= 12; $mes++){
+			
+				$mes = sprintf("%02d", $mes);
+			for($dia = 1; $dia <= 31; $dia++){
+				$dia = sprintf("%02d", $dia);
+
+
+
+				echo "{
+		\"value\": \"{$ano}-{$mes}-{$dia}\"
+	},";
+			}
+		}
+	}
+*/
+//For time
+/*
+	for($hour = 0; $hour <= 23; $hour++){
+				$hour = sprintf("%02d", $hour);
+		for($min = 0; $min <= 59; $min++){
+			
+				$min = sprintf("%02d", $min);
+			for($sec = 0; $sec <= 59; $sec++){
+				$sec = sprintf("%02d", $sec);
+
+
+
+				echo "{
+		\"value\": \"{$hour}-{$min}-{$sec}\"
+	},";
+			}
+		}
+	}
+*/
+
+//for year
+/*
+	for($year = 1990; $year <= 2016; $year++){
+		echo "{
+		\"value\": \"{$year}\"
+	},";
+	}
+*/
+?>
