c488720bf4825b69d789c8d2db73fb37032bb746
leozinho0_w <leandroviana28@gmail.com>
2016-11-15 20:29:58 -0200

    Correção do DS date. Adicionado ANO BISEXTO emes com 30 dias;

--- data_generator.php
+++ data_generator.php
@@ -2,24 +2,46 @@
 
 
 //for time
-/*
+$bissexto = 4;
+
 	for($ano = 1992; $ano <= 2016; $ano++){
 		for($mes = 1; $mes <= 12; $mes++){
 			
-				$mes = sprintf("%02d", $mes);
-			for($dia = 1; $dia <= 31; $dia++){
-				$dia = sprintf("%02d", $dia);
+			$mes = sprintf("%02d", $mes);
 
+			if($mes == '04' || $mes == '06' || $mes == '09' || $mes == '11'){
+				for($dia = 1; $dia <= 30; $dia++){
+					$dia = sprintf("%02d", $dia);
 
+					echo "{\"value\":\"{$ano}-{$mes}-{$dia}\"},<br>";
+				}
+			}else if($mes == '02' && $bissexto == 4){
+				for($dia = 1; $dia <= 29; $dia++){
+					$dia = sprintf("%02d", $dia);
 
-				echo "{
-		\"value\": \"{$ano}-{$mes}-{$dia}\"
-	},";
+					echo "{\"value\":\"{$ano}-{$mes}-{$dia}\"},<br>";
+				}
+
+				$bissexto = 0;
+			}else if($mes == '02' && $bissexto != 4){
+				for($dia = 1; $dia <= 28; $dia++){
+					$dia = sprintf("%02d", $dia);
+
+					echo "{\"value\":\"{$ano}-{$mes}-{$dia}\"},<br>";
+				}
+			}else{
+				for($dia = 1; $dia <= 31; $dia++){
+					$dia = sprintf("%02d", $dia);
+
+					echo "{\"value\":\"{$ano}-{$mes}-{$dia}\"},<br>";
+				}
 			}
 		}
+		$bissexto++;
 	}
-*/
+
 //For time
+/*
 	for($hour = 0; $hour <= 23; $hour++){
 				$hour = sprintf("%02d", $hour);
 		for($min = 0; $min <= 59; $min++){
@@ -36,7 +58,7 @@
 			}
 		}
 	}
-
+*/
 //for year
 /*
 	for($year = 1990; $year <= 2016; $year++){
