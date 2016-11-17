3dd2d648338d5890b68b3d9d5339ba627cff7082
leozinho0 <leandroviana28@gmail.com>
2016-11-17 17:07:49 -0300

    Adicionado Modo Debug

--- index.php
+++ index.php
@@ -6,104 +6,9 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<script src="js/jquery.js"></script>
-	<style>
-		body{
-			color: #333333;
-			font-family: Century Gothic, sans-serif;
-			background-color: #E0E0E0;
-			margin: 0;
-		}
-		.box_logo{
-			padding: 20px;
-			text-align: center;
-			margin: 0 auto;
-			width: 300px;
-			font-family: "Arial Rounded MT Bold", "Sans-serif";
-			font-size: 30px;
-			color: #ECF0F1;
-			text-shadow: 1px 1px 10px #000;
-		}
-		.box_center{
-			background-color: #ECF0F1;
-			text-align: right;
-			margin: 10px auto;
-			border-radius: 5px;
-			border: 1px solid #95A5A6;
-			padding: 10px;
-			width: 400px;
-			box-shadow: 3px 4px 10px #95A5A6;
-		}
-		.div_btn{
-			background-color: #D1D5D8;
-			width: 100px;
-			margin: 10px auto;
-			border-radius: 5px;
-			text-align: center;
-			padding: 4px;
-		}
-		.div_btn:hover{
-			box-shadow: 3px 4px 10px #95A5A6;
-			cursor: pointer;
-		}
-		.div_btn:active{
-			background-color: #CDC9C9;
-		}
-		.box_error{
-			margin: 10px auto;
-			border-radius: 5px;
-			border: 1px solid #95A5A6;
-			padding: 10px;
-			width: 400px;
-		    color: #D8000C;
-		    background-color: #FFBABA;
-			box-shadow: 3px 4px 10px #95A5A6;
-			text-align: center;
-			font-size: 15px;
-		}
-		.input{
-			width: 90%;
-		}
-		select{
-			width: 100%;	
-		}
-		#div_block{
-			display: none;
-			position: fixed;
-		    width: 100%;
-		    height: 100%;
-		    left: 0;
-		    top: 0;
-		}
-		#div_loading{
-			display: none;
-			margin: 10px auto;
-			border-radius: 5px;
-			border: 1px solid #95A5A6;
-			padding: 10px;
-			width: 400px;
-			background-color: #ECF0F1;
-			box-shadow: 3px 4px 10px #95A5A6;
-			text-align: center;
-			font-size: 15px;
-		}
-		#div_message{
-			display: none;
-			margin: 10px auto;
-			border-radius: 5px;
-			border: 1px solid #95A5A6;
-			padding: 10px;
-			width: 400px;
-			color: #4F8A10;
-    		background-color: #DFF2BF;
-			box-shadow: 3px 4px 10px #95A5A6;
-			text-align: center;
-			font-size: 15px;
-		}
-		table{
-			table-layout: fixed;
-			width: 100%;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- JS Functions Import -->
+	<script src="js/scripts.js"></script>
 </head>
 <body>
 	<div id="" class="box_logo">
@@ -193,7 +98,7 @@
 		<div>
 			<table>
 				<tr>
-					<td>Quantidade de registros</td>
+					<td>Quantidade de Registros</td>
 					<td>
 						<select name="" id="qtd_insert">
 							<option value="1" selected>1</option>
@@ -210,6 +115,13 @@
 						</select>
 					</td>
 				</tr>
+				<tr>
+					<td>Modo Debug</td>
+					<td>
+						<input type="radio" name="debug" value="on" id="debug">On
+						<input type="radio"  name="debug" checked="checked">Off
+					</td>
+				</tr>
 			</table>
 		</div>
 		<div class="div_btn" onclick="js_insert();">Inserir</div>
@@ -221,103 +133,5 @@
 		<img src="http://s1.ticketm.net/tm/en-us/img/sys/1000/gray75_polling.gif" alt="" width="30px"/> <br>
 		<span>Inserindo dados... Aguarde!</span>
 	</div>
-	<script>
-		function js_listBases(){
-			var d = 'sgbd=' + document.getElementById("conn_sgbd").value + '&adress=' + document.getElementById("conn_adress").value + '&user=' + document.getElementById("conn_user").value + '&password=' + document.getElementById("conn_password").value + "&id=2";
-			$.ajax({
-				type: 'POST',
-				url: 'conn_valid.php',
-				data: d,
-				success: function(ds){
-					arr = JSON.parse(ds);
-
-					$("#id_bases").html('');
-
-					$("#id_bases").append("<option value=''></option>");
-					for(it=0; it<arr.length; it++)
-					{
-						$("#id_bases").append("<option value='"+ arr[ it ].Database +"'>"+ arr[ it ].Database +"</option>");
-					}
-
-					$('#div_dados').show();
-					$('#div_bases').show();
-				}
-			});
-		}
-	</script>
-	<script>
-		function js_listTables(){
-			var d = 'sgbd=' + document.getElementById("conn_sgbd").value + '&adress=' + document.getElementById("conn_adress").value + '&user=' + document.getElementById("conn_user").value + '&password=' + document.getElementById("conn_password").value + "&base=" + document.getElementById("id_bases").value + "&id=3";
-			$.ajax({
-				type: 'POST',
-				url: 'conn_valid.php',
-				data: d,
-				success: function(ds){
-					arr = JSON.parse(ds);
-
-					$("#id_tables").html('');
-					for(it=0; it<arr.length; it++)
-					{
-						$("#id_tables").append("<option value='"+ arr[it][0] +"'>"+ arr[it][0] +"</option>");
-					}
-					$('#div_tabelas').show();
-				}
-			});
-		}
-	</script>
-	<!--ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI
-	ESTOU AQUI -->
-	<script>
-		function js_insert(){
-			//APAGAR ISSO
-			var quantidade = document.getElementById("qtd_insert").value;
-			//APAGAR ESSA MERDS
-
-			var d = 'sgbd=' + document.getElementById("conn_sgbd").value + '&adress=' + document.getElementById("conn_adress").value + '&user=' + document.getElementById("conn_user").value + '&password=' + document.getElementById("conn_password").value + "&base=" + document.getElementById("id_bases").value + "&table=" + document.getElementById("id_tables").value + "&qtd=" + document.getElementById("qtd_insert").value +"&id=4";
-			$('#div_message').hide();
-			$('#div_block').show();
-			$('#div_loading').fadeIn();
-			$.ajax({
-				type: 'POST',
-				url: 'conn_valid.php',
-				data: d,
-				success: function(ds){
-					$('#div_block').hide();
-					$('#div_loading').hide();
-					var mensagem = quantidade+" registro(s) inserido(s) com sucesso!";
-					$('#div_message').html(ds);
-					$('#div_message').fadeIn();
-				}
-			});
-		}
-	</script>
-	<script>
-		function js_conn(){
-			var d = 'sgbd=' + document.getElementById("conn_sgbd").value + '&adress=' + document.getElementById("conn_adress").value + '&user=' + document.getElementById("conn_user").value + '&password=' + document.getElementById("conn_password").value + "&id=1";
-			$.ajax({
-				type: 'POST',
-				url: 'conn_valid.php',
-				data: d,
-				success: function(ds){
-					if(ds == "y"){
-						$('#div_error').hide();
-						js_listBases();
-					}else {
-						$('#div_error').html(ds);
-						$('#div_error').fadeIn();
-					}
-				}
-			});
-		}
-	</script>
 </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 function js_listBases(){
 	var d = 'sgbd=' + document.getElementById("conn_sgbd").value + '&adress=' + document.getElementById("conn_adress").value + '&user=' + document.getElementById("conn_user").value + '&password=' + document.getElementById("conn_password").value + "&id=2";
 	$.ajax({
@@ -37,14 +35,17 @@ function js_listTables(){
 				$("#id_tables").append("<option value='"+ arr[it][0] +"'>"+ arr[it][0] +"</option>");
 			}
 			$('#div_tabelas').show();
-			$('#btn_insert').show();
 		}
 	});
 }
 function js_insert(){
+	//APAGAR ISSO
+	var quantidade = document.getElementById("qtd_insert").value;
+	var debug = document.getElementById("debug").checked;
+	//APAGAR ESSA MERDS
+
 	var d = 'sgbd=' + document.getElementById("conn_sgbd").value + '&adress=' + document.getElementById("conn_adress").value + '&user=' + document.getElementById("conn_user").value + '&password=' + document.getElementById("conn_password").value + "&base=" + document.getElementById("id_bases").value + "&table=" + document.getElementById("id_tables").value + "&qtd=" + document.getElementById("qtd_insert").value +"&id=4";
-	$('#div_message_error').hide();
-	$('#div_message_success').hide();
+	$('#div_message').hide();
 	$('#div_block').show();
 	$('#div_loading').fadeIn();
 	$.ajax({
@@ -52,16 +53,15 @@ function js_insert(){
 		url: 'conn_valid.php',
 		data: d,
 		success: function(ds){
-			if(ds == 1){
-				$('#div_block').hide();
-				$('#div_loading').hide();
-				$('#div_message_success').html("Dados Inseridos com Sucesso!");
-				$('#div_message_success').fadeIn();
+			$('#div_block').hide();
+			$('#div_loading').hide();
+			var mensagem = quantidade+" registro(s) inserido(s) com sucesso!";
+			if(debug){
+				$('#div_message').html(ds);
+				$('#div_message').fadeIn();
 			}else{
-				$('#div_block').hide();
-				$('#div_loading').hide();
-				$('#div_message_error').html(ds);
-				$('#div_message_error').fadeIn();
+				$('#div_message').html(mensagem);
+				$('#div_message').fadeIn();
 			}
 		}
 	});
@@ -73,12 +73,12 @@ function js_conn(){
 		url: 'conn_valid.php',
 		data: d,
 		success: function(ds){
-			if(ds == 1){
-				$('#div_connection_error').hide();
+			if(ds == "y"){
+				$('#div_error').hide();
 				js_listBases();
 			}else {
-				$('#div_connection_error').html(ds);
-				$('#div_connection_error').fadeIn();
+				$('#div_error').html(ds);
+				$('#div_error').fadeIn();
 			}
 		}
 	});
