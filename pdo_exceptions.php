8182205f08e67c00b5c11a1701c9b3d299faeb15
leozinho0 <leandroviana28@gmail.com>
2016-11-28 12:22:33 -0300

    pdo_exceptions file

--- pdo_exceptions.php
+++ pdo_exceptions.php
@@ -3,11 +3,18 @@
 try{
 	$conn = new PDO("mysql:host=127.0.0.1","root","root");
 }catch(PDOException $e){
-	echo $e;
+	echo $e->getMessage();
 }
-try{
-	$conn->query("sadasd");
-}catch(PDOException $e){
-	echo $e;
-}
+
+/*
+	$conn->query("use yp;");
+	$conn->query("INSERT INTO infeedweights VALUES(-62, '2005-04-10', 5171.46649);");
+
+	print_r($conn->errorInfo());
+*/
+
+
+
+
+
 ?>
